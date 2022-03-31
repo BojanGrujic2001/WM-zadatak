c7bd304745bc0d2f32b68811b41d7fd095c09137
BojanGrujic2001 <bojan.grujic200@gmail.com>
2022-03-31 20:50:37 +0200

    search and carousel left

--- JavaScript.js
+++ JavaScript.js
@@ -3,12 +3,5 @@ function top(){
   document.documentElement.scrollTop = 0;
 }
 
-function Meni() {
-    var x = document.getElementsByClassName("menu").item(0);
-    if (x.className == "menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu";
-    }
-}
+
 
